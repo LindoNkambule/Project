38400ae8620725e4d3cfcf833d4310f8c708146d
LindoNkambule <lindonkambule116@gmail.com>
2019-05-17 14:27:28 +0200

    updated Docker2Singularity.sh script

--- Docker2Singularity.sh
+++ Docker2Singularity.sh
@@ -2,7 +2,10 @@
 #docker run singularityware/docker2singularity
 
 docker run -v /var/run/docker.sock:/var/run/docker.sock \
--v /Users/lindokuhle/Desktop/Project:/output \
+-v /path/to/directory:/output \
 --privileged -t --rm \
 singularityware/docker2singularity \
 --name honours_project ubuntu:18.04
+
+#-v specify the path in which you want to save the singularity image to
+#--name specify the name you want to give to the image
